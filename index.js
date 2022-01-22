8f28a369fa881f77b20851face18728b6f9d892c
natalie <nataliemae263@gmail.com>
2022-01-21 21:02:54 -0600

    startercode

---
+++ index.js
@@ -0,0 +1,43 @@
+//* Activity - Employee Rewards - Starter Code */
+/* Construct conditional statements that meet the requirements: */
+/* ================================== */
+
+/*1. If the employee has 1 year with the company, 1 new checking account: Output: 'Ship 50.00 Amazon Gift Card'
+Otherwise output:'Incentives not met'
+
+/*2. If the employee has 2 years with the company, 3+ new checking accounts:  Output: 'Ship 50.00 Amazon Gift Card'
+Otherwise output:'Ship $100.00 Visa Gift Card'
+
+/*3. If the employee has 3 years with the company, 2+ new checking accounts:  Output: 'Ship 700.00 Visa Gift Card'
+Otherwise output:'Ship $350.00 Visa Gift Card'
+
+/*4. If the employee has 5+ years with the company, 1+ new checking accounts:  Output: 'Ship 3500.00 Visa Gift Card'
+Otherwise output:'Ship set of knives'
+*/
+
+
+// variable declaration
+let associateBanker = [56123, 0, 0]; //employeeId, checking, years
+
+let empId = associateBanker[0]       //employee Id number
+let empChecking = associateBanker[1] //number of new checking 
+let empYears = associateBanker[2];   //years employed
+
+//continue same pattern/naming convention for each banker
+//leaving only one variable declaration section uncommented each 'Run'
+
+/* 
+let empId = seniorBanker[0]       
+let empChecking = seniorBanker[1]
+let empYears = seniorBanker[2];   
+*/
+
+
+//<--Your code goes here-->
+
+
+//to output when activity is complete
+console.log(abEmpId + " : Processed")
+
+    
+   
